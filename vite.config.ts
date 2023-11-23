b81f5a648f1d39d6f1637ff1803fba1b66feee19
belozerov-egor <belaz.egor@gmail.com>
2023-11-23 21:53:25 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'path'
+
+import { defineConfig } from 'vite'
+
+import { dependencies, devDependencies } from './package.json'
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['es'],
+      name: 'new-ui-kit-lib',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['jsx-runtime.js', ...Object.keys(dependencies), ...Object.keys(devDependencies)],
+    },
+    sourcemap: true,
+    target: 'esnext',
+  },
+})
