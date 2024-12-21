bc774fc3632a55b7005273efd849bc94effd18f0
Егор Белозеров <BelozerovES@it.mos.ru>
2024-12-22 00:58:46 +0300

    up ver

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,39 @@ export default defineConfig({
     cssCodeSplit: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      fileName: 'index',
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
       formats: ['es'],
-      name: 'new-ui-kit-lib',
     },
     rollupOptions: {
       external: ['jsx-runtime.js', ...Object.keys(dependencies), ...Object.keys(devDependencies)],
+      input: {
+        index: resolve(__dirname, 'src/index.ts'),
+      },
       output: {
+        assetFileNames: assetInfo => {
+          const { name } = assetInfo
+
+          if (name && name.endsWith('.css')) {
+            return `css/[name][extname]`
+          }
+
+          return 'assets/[name][extname]'
+        },
+        dir: 'dist',
+        entryFileNames: ({ facadeModuleId }) => {
+          if (!facadeModuleId) {
+            return '[name].js'
+          }
+          const moduleName = facadeModuleId.split('/src/')[1]
+
+          if (!moduleName) {
+            return '[name].js'
+          }
+
+          return moduleName.replace('.tsx', '.js').replace('.ts', '.js')
+        },
+        exports: 'named',
+        format: 'es',
         preserveModules: true,
         preserveModulesRoot: 'src',
       },
