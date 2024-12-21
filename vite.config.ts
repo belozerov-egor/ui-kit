51762d0c5dc92992a4500ce86bf8e511b7e6c3ba
Егор Белозеров <BelozerovES@it.mos.ru>
2024-12-22 00:48:46 +0300

    fix styles

--- vite.config.ts
+++ vite.config.ts
@@ -2,33 +2,33 @@ import { resolve } from 'path'
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import svgr from 'vite-plugin-svgr'
 
 import { dependencies, devDependencies } from './package.json'
-// https://vitejs.dev/config/
+
 export default defineConfig({
   build: {
+    cssCodeSplit: true,
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
-      // the proper extensions will be added
       fileName: 'index',
       formats: ['es'],
       name: 'new-ui-kit-lib',
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['jsx-runtime.js', ...Object.keys(dependencies), ...Object.keys(devDependencies)],
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+      },
     },
     target: 'esnext',
   },
   css: {
-    devSourcemap: true,
     modules: {
       generateScopedName: '[name]_[local]_[hash:base64:5]',
+      scopeBehaviour: 'local',
     },
   },
-  plugins: [libInjectCss(), react(), svgr()],
+  plugins: [react(), svgr()],
 })
