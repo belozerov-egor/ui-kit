6ede0663f7f498a805dca160ce2eab2bafceba04
Егор Белозеров <BelozerovES@it.mos.ru>
2024-12-22 23:19:52 +0300

    fix css

--- vite.config.ts
+++ vite.config.ts
@@ -2,59 +2,31 @@ import { resolve } from 'path'
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import svgr from 'vite-plugin-svgr'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 import { dependencies, devDependencies } from './package.json'
-
+// https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    cssCodeSplit: true,
     lib: {
+      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
-      fileName: (format, entryName) => `${entryName}.${format}.js`,
+      // the proper extensions will be added
+      fileName: 'index',
       formats: ['es'],
     },
     rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
       external: ['jsx-runtime.js', ...Object.keys(dependencies), ...Object.keys(devDependencies)],
-      input: {
-        index: resolve(__dirname, 'src/index.ts'),
-      },
-      output: {
-        assetFileNames: assetInfo => {
-          const { name } = assetInfo
-
-          if (name && name.endsWith('.css')) {
-            return `css/[name][extname]`
-          }
-
-          return 'assets/[name][extname]'
-        },
-        dir: 'dist',
-        entryFileNames: ({ facadeModuleId }) => {
-          if (!facadeModuleId) {
-            return '[name].js'
-          }
-          const moduleName = facadeModuleId.split('/src/')[1]
-
-          if (!moduleName) {
-            return '[name].js'
-          }
-
-          return moduleName.replace('.tsx', '.js').replace('.ts', '.js')
-        },
-        exports: 'named',
-        format: 'es',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-      },
     },
     target: 'esnext',
   },
   css: {
+    devSourcemap: true,
     modules: {
       generateScopedName: '[name]_[local]_[hash:base64:5]',
-      scopeBehaviour: 'local',
     },
   },
-  plugins: [react(), svgr()],
+  plugins: [libInjectCss(), react()],
 })
